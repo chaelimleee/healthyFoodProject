ab0bd6c00346bc388f2c1336b038a7715f4d3de1
39150 <carringkhk@naver.com>
2024-03-26 14:26:36 +0900

    modified:   src/main/java/com/javateam/healthyFoodProject/HealthyFoodProjectApplication.java 	modified:   src/main/java/com/javateam/healthyFoodProject/ServletInitializer.java

--- src/main/java/com/javateam/healthyFoodProject/ServletInitializer.java
+++ src/main/java/com/javateam/healthyFoodProject/ServletInitializer.java
@@ -7,7 +7,7 @@ public class ServletInitializer extends SpringBootServletInitializer {
 
 	@Override
 	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-		return application.sources(healthyFoodProjectApplication.class);
+		return application.sources(HealthyFoodProjectApplication.class);
 	}
 
 }
