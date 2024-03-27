1f85cf5ada0a12c999edd936426a1881d3f98463
chaelim lee <cofla1007@gmail.com>
2024-03-27 16:26:10 +0900

    new upload_0327_카테고리 수정

--- src/main/java/com/javateam/healthyFoodProject/HealthyFoodProjectApplication.java
+++ src/main/java/com/javateam/healthyFoodProject/HealthyFoodProjectApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class healthyFoodProjectApplication {
+public class HealthyFoodProjectApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(healthyFoodProjectApplication.class, args);
+		SpringApplication.run(HealthyFoodProjectApplication.class, args);
 	}
 
 }
