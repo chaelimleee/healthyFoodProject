fbec7281b0928e9bde9fb5b6a5794b722c121bd0
chaelimleee <112537126+chaelimleee@users.noreply.github.com>
2024-05-07 15:07:27 +0900

    Update FoodListController.java

--- src/main/java/com/javateam/healthyFoodProject/controller/food/FoodListController.java
+++ src/main/java/com/javateam/healthyFoodProject/controller/food/FoodListController.java
@@ -32,10 +32,10 @@ public class FoodListController {
 
 	@GetMapping("list.do")
 	public String list(@RequestParam(value = "currPage", defaultValue = "1") int currPage,
-			@RequestParam(value = "limit", defaultValue = "10") int limit,
+			@RequestParam(value = "limit", defaultValue = "20") int limit,
 			Model model) {
 
-		log.info("게시글 목록");
+		log.info("food 게시글 목록");
 		List<FoodVO> foodList = new ArrayList<>();
 		
 
@@ -53,16 +53,17 @@ public class FoodListController {
 //		foodList = foodDao.findAllByOrderByFoodCodeDesc();
 //		Pageable pageable = PageRequest.of(currPage - 1, limit);
 //		foodList = foodService.findAllByOrderByFoodCode();
-		log.info("foodList 리스트 확인 ==> " + foodList.get(0).toString());
+		//log.info("foodList 리스트 확인 ==> " + foodList.get(0).toString());
 
 		// 총 페이지 수
 		// int maxPage=(int)((double)listCount/limit+0.95); //0.95를 더해서 올림 처리
 		int maxPage = PageVO.getMaxPage(listCount, limit);
 		// 현재 페이지에 보여줄 시작 페이지 수 (1, 11, 21,...)
 		// int startPage = (((int) ((double)currPage / 10 + 0.9)) - 1) * 10 + 1;
-		int startPage = PageVO.getStartPage(currPage, limit);
+		//int startPage = PageVO.getStartPage(currPage, limit);
+		int startPage = currPage == 1 ? 1 : PageVO.getStartPage(currPage, limit);//0430 수정. 
 		// 현재 페이지에 보여줄 마지막 페이지 수(10, 20, 30, ...)
-		int endPage = startPage + 10;
+		int endPage = startPage + 20;
 
 		if (endPage > maxPage)
 			endPage = maxPage;
@@ -84,6 +85,12 @@ public class FoodListController {
 		// 0404 leee 페이지네이션 위해서 현재 페이지에 보여줄 시작 페이지, 마지막 페이지 list.html에 보냄
 		model.addAttribute("startPage", startPage);
 		model.addAttribute("endPage", endPage);
+		
+		// title 0430 레시피
+		model.addAttribute("pageTitle", "건강식 레시피");
+		model.addAttribute("bgImg", "food4.jpg");
+		
+		log.info("foodList end : ");
 
 		return "/food/food_list";
 	} //
