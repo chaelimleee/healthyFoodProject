a9f0bfa03db4b53a78c52986b99af5825c2fc0f5
chaelimleee <112537126+chaelimleee@users.noreply.github.com>
2024-05-07 14:45:56 +0900

    Update SecurityConfig.java

--- src/main/java/com/javateam/healthyFoodProject/config/SecurityConfig.java
+++ src/main/java/com/javateam/healthyFoodProject/config/SecurityConfig.java
@@ -51,10 +51,12 @@ public class SecurityConfig {
 		// swagger 항목 예외(열외) 추가 :
 		// 참고) /v2/api-docs : swagger의 전체적인 환경설정 정보를 JSON 형식으로 보여주는 페이지
 		// /v2/api-docs, /swagger-resources/**, /swagger/**, swagger-ui.html
-		// axios 항목 예외 추가
+		// axios 항목 예외 추가 
+		// /swagger/** ==> /swagger-ui/** 변경함. 
+		// /swagger-ui.html  ==>  /swagger-ui/index.html 변경함. 
 		return (web) -> web.ignoring().requestMatchers("/css/**", "/webjars/**", "/img/**",
-				"/images/**", "/js/**", "/v2/api-docs", "/swagger-resources/**", "/swagger/**", "/swagger-ui.html",
-				"/axios/**", "/bootstrap-icons/**", "/bootstrap/**",
+				"/images/**", "/js/**", "/v2/api-docs", "/swagger-resources/**", "/swagger-ui/**", "/swagger-ui/index.html",
+				"/axios/**", "/bootstrap-icons/**", "/bootstrap/**", "/lib/**",
 				"/summernote/**");
 	}
 
@@ -76,13 +78,16 @@ public class SecurityConfig {
 				// .requestMatchers("/", "/css/**", "/webjars/**", "/images/**", "/js/**",
 				// "/axios/**", "/bootstrap-icons/**").permitAll()
 				.requestMatchers("/","/demo").permitAll()
-				.requestMatchers("/swagger-resources/**", "/swagger/**", "/swagger-ui.html").permitAll()
+				
+				// /swagger/** ==> /swagger-ui/** 변경함. 
+				// /swagger-ui.html  ==>  /swagger-ui/index.html 변경함. 
+				.requestMatchers("/swagger-resources/**", "/swagger-ui/**", "/swagger-ui/index.html").permitAll()
 				.requestMatchers("/member/hasFld/**", "/member/view.do").permitAll()
 				.requestMatchers("/member/update.do", "/member/updateProc.do").authenticated()
 				.requestMatchers("/member/updateSess.do", "/member/updateSessProc.do").authenticated()
 				.requestMatchers("/member/join.do", "/member/joinProc.do", "/member/joinProcRest.do").permitAll()
 				.requestMatchers("/member/updateRoles/**", "/member/changeMemberState/**",
-						"/member/updateMemberByAdmin/**", "/member/deleteMemberByAdmin/**")
+						"/member/updateMemberByAdmin/**", "/member/deleteMemberByAdmin/**","/admin/adminQna.do")
 				.authenticated()
 				.requestMatchers("/board/replyWrite.do").permitAll()
 				.requestMatchers("/admin/**").hasAuthority("ROLE_ADMIN")
@@ -92,27 +97,39 @@ public class SecurityConfig {
         //240405-es-song- photo_board 추가(permitAll(),authenticated())
 		// /board/view.do/** 비회원만 볼 수 있는 상황. 로그인 한 사용자는 볼 수 없는 것 수정해야함. 0415 leee
 		//  게시글 보기 해결함!!!! permitAll을 view.do만 따로 줬더니 잘 들어가짐 !!!!!0415 leee
-        .requestMatchers("/board/view.do/**").permitAll()
+				.requestMatchers("/board/view.do/**").permitAll()
         		.requestMatchers("/board/list.do/**","/board/searchList.do",
                 "/board/image", "/board/image/**",
                 "/board/getRepliesAll.do","/board/download/**",
-                "/photo_board/view.do/**","/photo_board/list.do/**","/photo_board/searchList.do",
+                "/photo_board/photo_view.do/**","/photo_board/list.do","/photo_board/searchList.do",
                 "/photo_board/image", "/photo_board/image/**",
                 "/photo_board/getRepliesAll.do").permitAll()
+        		.requestMatchers(
+        		"/qna/qna_view.do","/qna/image", "/qna/image/**",
+                "/qna/getRepliesAll.do",
+                "/qna/qna_view.do/**","/qna/list.do/**","/qna/searchList.do",
+                "/qna/image", "/qna/image/**",
+                "/qna/getRepliesAll.do").permitAll()
 
-        .requestMatchers("/board/write.do","/board/writeProc.do",
-                 "/board/update.do", "/board/updateProc.do",
-                 "/board/replyWrite.do",
-                  "/board/replyUpdate.do", 
-                 "/board/replyDelete.do",
-                 "/board/deleteProc.do",
-                 "/photo_board/write.do","/photo_board/writeProc.do",
-                 "/photo_board/update.do", "/photo_board/updateProc.do",
-                 "/photo_board/replyWrite.do",
-                 "/photo_board/replyUpdate.do", 
-                 "/photo_board/replyDelete.do",
-                 "/photo_board/deleteProc.do"
-                 ).authenticated()
+        		.requestMatchers("/board/write.do","/board/writeProc.do",
+                        "/board/update.do", "/board/updateProc.do",
+                        "/board/replyWrite.do",
+                        "/board/replyUpdate.do",
+                        "/board/replyDelete.do",
+                        "/board/deleteProc.do",
+                        "/photo_board/write.do","/photo_board/writeProc.do",
+                        "/photo_board/update.do", "/photo_board/updateProc.do",
+                        "/photo_board/replyWrite.do",
+                        "/photo_board/replyUpdate.do",
+                        "/photo_board/replyDelete.do",
+                        "/photo_board/deleteProc.do",
+                        "/qna/write.do","/qna/writeProc.do",
+                        "/qna/update.do", "/qna/updateProc.do",
+                        "/qna/replyWrite.do",
+                        "/qna/replyUpdate.do",
+                        "/qna/replyDelete.do",
+                        "/qna/deleteProc.do"
+                        ).authenticated()
         
         .requestMatchers("/sasang/**").authenticated()
         
@@ -122,7 +139,7 @@ public class SecurityConfig {
 				.loginPage("/login")
 				.usernameParameter("memberEmail")
 				.passwordParameter("memberPw")
-				.defaultSuccessUrl("/welcome")
+				.defaultSuccessUrl("/")
 				.failureUrl("/loginError")
 				// .successHandler(new CustomAuthenticationSuccess()) // 로그인 성공 핸들러
 				// .failureHandler(new CustomAuthenticationFailure()) // 로그인 실패 핸들러
