17db3f14d561c57a7332d22e4f31ffd99f5b984a
chaelimleee <112537126+chaelimleee@users.noreply.github.com>
2024-05-07 15:10:18 +0900

    Update SasangController.java

--- src/main/java/com/javateam/healthyFoodProject/controller/sasang/SasangController.java
+++ src/main/java/com/javateam/healthyFoodProject/controller/sasang/SasangController.java
@@ -62,11 +62,15 @@ public class SasangController {
 	 * @return
 	 */
 
-	@GetMapping("/sasangHome.do")
+	@GetMapping("/sasang.do")
 	public String sasangPage(Model model) {
 		
 		log.info("sasang 첫 페이지 ");
 		model.addAttribute("arg", "인자");
+		
+		// title 0430 사상체질
+		model.addAttribute("pageTitle", "<span style='color:#fff;'>사상체질</span>");
+		model.addAttribute("bgImg", "sasang2.jpg");
 		
 		return "sasang/sasang_home";
 	}
@@ -77,6 +81,10 @@ public class SasangController {
 		log.info("sasang 첫 페이지 ");
 		model.addAttribute("arg", "인자");
 		
+		// title 0430 사상체질
+		model.addAttribute("pageTitle", "<span style='color:#fff;'>사상체질</span>");
+		model.addAttribute("bgImg", "sasang2.jpg");
+		
 		return "sasang/sasang_list";
 	}
 	
@@ -85,72 +93,29 @@ public class SasangController {
 		
 		log.info("sasang 결과 페이지");
 		
-		List<String> sasangFoodListName = new ArrayList<>();
+		//0424 leee 추가 추천 레시피 뜨게 함. 
+		List<FoodVO> sasangFoodListName = new ArrayList<>();
 		sasangFoodListName = foodService.findSasangGoodIngredientMainBySasangName(sasang);
 		
-		log.info("sasang 음식 잘 나오는지 확인 >> " + sasangFoodListName.get(0));
+		//log.info("sasang 재료 잘 나오는지 확인 >> " + sasangFoodListName.get(0).toString());
 		
-		List<FoodVO> sasangFoodList = foodService.findByFoodIngredientMainInsideIn(sasangFoodListName);
-		log.info("sasangFoodList 음식 잘 나오는지 확인 >> " + sasangFoodList.get(0));
+		// 재료가 컬럼 안에 하나만 있는 것만 가져옴 
+//		List<FoodVO> sasangFoodList = foodService.findByFoodIngredientMainInsideIn(sasangFoodListName);
 		
-		model.addAttribute("sasangFoodList", sasangFoodList);
+		//재료를 잘 뺴왔으면 food_tbl 의 재료들에서 포함하는 레시피들을 모두 가져와야함.0424 
+		//List<FoodVO> sasangFoodList = foodService.findByFoodIngredientMainInsideLike(sasang);
+		//log.info("sasangFoodList 음식 잘 나오는지 확인 >> " + sasangFoodListName.get(0));
 		
-		return "sasang/sasang_result";
+		model.addAttribute("sasangFoodList", sasangFoodListName);
+		model.addAttribute("sasangName", sasang);
+		
+		// title 0430 사상체질
+		model.addAttribute("pageTitle", "<span style='color:#fff;'>사상체질</span>");
+		model.addAttribute("bgImg", "sasang2.jpg");
+		
+		return "sasang/sasang_Result";
 	}
 	
 	
-//	@GetMapping("/saveSasang.do/{memberEmail}/{sasang}")
-////	@GetMapping("/sasangResult.do/{memberEmail}/{sasang}")
-//	@ResponseBody
-//	public ResponseEntity<String> updateMemberSasang(@PathVariable("memberEmail") String memberEmail,
-//													@PathVariable("sasang") String sasang) {
-//		
-//		log.info("sasang 확인 >> " + sasang +" , memberEmail 확인 >> "+ memberEmail); 
-//		
-//		ResponseEntity<String> responseEntity = null;
-//		String msg = "";
-//		
-//		String sasangArray[] = {"소음인","소양인","태음인","태양인"};
-//		List<String> sasangList = Arrays.asList(sasangArray);
-//		
-//		try {
-//			// 사상체질이 4가지 중에서 잘못 입력되면 
-//			if(sasangList.contains(sasang) == false) {
-//				
-//				msg = "회원정보 체질 수정에 실패하셨습니다.";
-//				responseEntity = new ResponseEntity<>(msg, HttpStatus.NO_CONTENT);
-//			} else {
-//			
-//				MemberDTO memberDTO = new MemberDTO();
-//				memberDTO.setMemberEmail(memberEmail);
-//				memberDTO.setMemberSasang(sasang);
-//				log.info("memberDTO 확인 >> " + memberDTO );
-//				
-//				boolean result = memberService.updateMemberSasang(memberDTO);
-//				
-//				log.info("--- result : {}", result);
-//				
-//				if (result == true) {
-//					// 중복된 아이디가 있음 : 성공 코드(200)
-//					msg = "회원정보 체질 수정에 성공하셨습니다.";
-//					responseEntity = new ResponseEntity<>(msg, HttpStatus.OK); 
-//				} else {
-//					// 중복된 아이디가 없음 : 실패 코드(204)
-//					msg = "회원정보 체질 수정에 실패하셨습니다.";
-//					responseEntity = new ResponseEntity<>(msg, HttpStatus.NO_CONTENT);
-//				}
-//			}
-//		} catch (Exception e) {
-//			log.error("updateMemberSasang error : {}", e);
-//			e.printStackTrace();
-//
-//			// 실패 코드(417) : 내부 서버 에러
-//			responseEntity = new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
-//		}
-//		
-//		return responseEntity;
-//	}
-//	
-//	
 	
 }
